af6db6d0fe9bb9993e09768b25263cbd05f344ed
Ashutosh Tiwari <141515182+ControllerKid@users.noreply.github.com>
2026-08-31 13:39:06 +0530

    chnaged groq models

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -13,11 +13,11 @@ load_dotenv()
 
 
 llm_search = ChatGroq(
-    model="llama-3.1-8b-instant"
+    model="openai/gpt-oss-120b"
 )
 
 llm_generation = ChatGroq(
-    model="llama-3.1-8b-instant"
+    model="openai/gpt-oss-120b"
 )
 
 #search agent 
